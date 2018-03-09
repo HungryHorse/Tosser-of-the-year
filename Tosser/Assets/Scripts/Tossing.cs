b1a1bb7b4af612bc442cf6c7119d18a502910ed9
HungryHorse <33032163+HungryHorse@users.noreply.github.com>
2018-03-09 14:24:20 +0000

    game

---
+++ Tosser/Assets/Scripts/Tossing.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tossing : MonoBehaviour {
+
+    public int power;
+    public float angle;
+    public GameObject Projectile;
+    public Transform spawnpoint;
+
+	void Update () {
+		
+	}
+
+    void Shoot()
+    {
+        Instantiate(Projectile, spawnpoint);
+    }
+}
